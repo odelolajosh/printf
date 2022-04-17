8bd4420b7735d2d2d1df174083e90b64a8081ba9
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-04-17 22:57:17 +0100

    add handler section for % and C

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdarg.h>
 
 /**
  * _printf - produces output according to a format
@@ -10,7 +10,28 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int len = 0;
+	unsigned int len = 0, i = 0;
+	char* out_string;
+	va_list args;
+	va_start(args, format);
 
+	while (format && format[i])
+	{
+		++i; ++len;
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				out_string[i] = '%';
+				continue;
+			}
+			else if (format[i + 1] == 'c')
+			{
+				char c = va_args(args, char);
+				out_string[i];
+			}
+		}
+		out_string[i] = format[i];
+	}
 	return (len);
 }
