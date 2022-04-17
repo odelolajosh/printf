de533c7f5a6f464819963b85cb9da6c305f4cc85
odelolajosh <oladapoodelola@gmail.com>
2022-04-17 19:20:31 +0000

    Add _printf file

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * _printf - produces output according to a format
+ *
+ * @format: given string
+ *
+ * Return: number of chars printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int len = 0;
+
+	return (len);
+}
